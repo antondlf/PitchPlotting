310d13240b6a6c30e4006c9e9d65d0b7213c0615
antondlf <72117387+antondlf@users.noreply.github.com>
2021-02-13 18:14:34 -0800

    Changed text_plot.py to add jitter to text

--- pitch_track/text_plot.py
+++ pitch_track/text_plot.py
@@ -17,30 +17,46 @@ def midpoint(x, y):
 
 def draw_text_plot(audio, textgrid, plot_path,  jitter = 0.001, text_jitter = 10):
 
-    pitch = parselmouth.Sound(audio).to_pitch()
     # Extract selected pitch contour, and
-    # replace unvoiced samples by NaN to not plot
     pitch_values = pitch.selected_array['frequency']
     np.trim_zeros(pitch_values)
+
     # Format textgrid
-    tg = tgio.openTextgrid(textgrid)
-    entryList = tg.tierDict["Sentence"].entryList  # Get all intervals
+    entryList = textgrid.tierDict["Sentence"].entryList  # Get all intervals
+
+    # Get time values rounded to correspond to correspond to textgrid values
     time = pitch.xs()
     time = (np.around(time, decimals=2))
-    invert_value = -1
+
+    # Color list for plotting
     color_list = ['b', 'r', 'g', 'm', 'c', 'k', 'y']
+
+    # Summary statistics to remove outliers and noise
+    deviation = np.std(pitch_values)
+    average = np.mean(pitch_values)
+
+    # Remove 0 values and values further than two standard deviations from the mean
     pitch_values[pitch_values == 0] = np.nan
-    #pitch_values[pitch_values >= 300] = np.nan
+    pitch_values[pitch_values >= average+(deviation*2.5)] = np.nan
+    pitch_values[pitch_values <= average-(deviation*2.5)] = np.nan
+
+    # Make sure there are enough colors for each word
     while len(color_list) < len(entryList):
         color_list = color_list *2
-    for interval, style in zip(entryList, color_list):
+
+    # Get jitter values for text
+    text_jitter = np.resize([20, 0], len(entryList))
+    print(text_jitter)
+
+    # Iterate through each interval in the textgrid together with the colors
+    for interval, style, t_jitter in zip(entryList, color_list, text_jitter):
         #plt.axvline(interval[0], linestyle='dotted')
         # Get start and end time for each boundary
         start, end = (np.where(time == round(interval[0], 2))[0], np.where(time == round(interval[1], 2))[0])
         # Place text at the start of each interval
         plt.text(
             round(interval[0], 2) + jitter,
-            max(pitch_values[start[0]:end[0]]) + text_jitter,
+            average+(deviation*2.5) + t_jitter,
             interval[2],
             fontsize=10,
             c = style
@@ -56,23 +72,6 @@ def draw_text_plot(audio, textgrid, plot_path,  jitter = 0.001, text_jitter = 10
     plt.ylim(0, pitch.ceiling)
     plt.ylabel("fundamental frequency [Hz]")
     plt.tick_params(axis='x', which='both')
-    plt.show()
     plt.savefig(plot_path)
     # Optionally pre-emphasize the sound before calculating the spectrogram snd.pre_emphasize()
-
-# snd = parselmouth.Sound('/Users/anton/desktop/Mama_recording.wav')
-# pitch = snd.to_pitch()
-# textgrid =  tgio.openTextgrid(
-#         "/Users/anton/desktop/mama_recording.TextGrid")
-
-# spectrogram = snd.to_spectrogram(maximum_frequency=8000.0)
-# plt.figure()
-# #draw_spectrogram(spectrogram)
-# #plt.twinx()
-#draw_text_plot(pitch(pitch, textgrid)#, jitter=0.5)
-# plt.xlim([snd.xmin, snd.xmax])
-#
-# tg = tgio.openTextgrid("/Users/anton/PycharmProjects/FlaskTutorial/Recordings/Chapter_1/Sentence1_dec.TextGrid")
-# entryList = tg.tierDict["Sentence"].entryList # Get all intervals
-#
-
+s
