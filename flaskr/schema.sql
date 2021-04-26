af8e65b3aa0d205875b4e8c1a9074871a97741f3
Antón <antondlf@mac.com>
2021-04-25 18:44:56 -0700

    Re-structured database for cleaner and more comprehensive data processing.

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -12,25 +12,32 @@ CREATE TABLE user (
 CREATE TABLE userdata (
     user_id TEXT UNIQUE NOT NULL,
     experimental_condition TEXT NOT NULL,
-    chapter_list TEXT NOT NULL
+    chapter_list TEXT NOT NULL --TODO: link with functionality for randomization
 );
 
 CREATE TABLE recordings (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
+  chapter_order TEXT NOT NULL, --TODO: clarify naming
   user_id INTEGER NOT NULL,
+  experimental_condition TEXT NOT NULL,
+  session_number TEXT NOT NULL,
+  trial_type TEXT NOT NULL, --Only three categories encoded (7 categories encoded together with session_number
+  sent_group TEXT NOT NULL,
+  sent_type TEXT NOT NULL, --TODO: check if this is easy to encode (maybe str.endswith('?'))
+  chapter_id TEXT NOT NULL, --TODO: come up with sentence identifiers (change to sent_id)
+  repetition TEXT NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  chapter_id TEXT NOT NULL,
-  chapter_order TEXT NOT NULL,
-  trial_id TEXT NOT NULL,
-  is_baseline TEXT NOT NULL,
+  trial_id TEXT NOT NULL, --TODO: trial id creation (record.py)
   FOREIGN KEY (user_id) REFERENCES user (id)
 );
 
 CREATE TABLE chapters (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  sent_group TEXT NOT NULL --TODO: decide if this should be encoded (prosodic groups)
+  sent_type TEXT NOT NULL,
   chapter_title TEXT NOT NULL,
+  text TEXT NOT NULL,
   audio_path TEXT NOT NULL,
   textplot_path TEXT NOT NULL,
-  text TEXT NOT NULL
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 );
