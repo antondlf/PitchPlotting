503a87a6238753333d1558b9e7205edbf8e05ba8
Antón <antondlf@mac.com>
2021-05-01 03:00:02 -0700

    Variable renaming and query adaptations both to sql schema. Collapsed condition checking if baseline to one set of conditions.

--- flaskr/data_management.py
+++ flaskr/data_management.py
@@ -11,11 +11,11 @@ def get_unique_id():
     return str(uuid.uuid1())
 
 
-def init_chapters(database):
+def init_chapters(database): # TODO: make only one class of sentence
     """Take chapter titles from Recordings directory and
     create a chapter for each file"""
     recordings_dir = os.path.join(current_app.root_path, '../Recordings')
-    title = None
+    sent_id = None
     text = None
     sentence_text_exists = False
     # Get chapter directory
@@ -25,7 +25,7 @@ def init_chapters(database):
 
         # Check that it is a directory
         if os.path.isdir(chap_directory):
-            title = dirname
+            sent_id = dirname
             item_pair = os.listdir(chap_directory)
             print(item_pair)
             item_pair.sort(key=lambda f: f.rsplit('.')[-1], reverse=True)
@@ -33,33 +33,40 @@ def init_chapters(database):
             print(item_pair)
             for file in item_pair:
 
+                sent_group = 'Group' # TODO: figure out where to source
+
                 if file.endswith('.txt'):
                     with open(os.path.join(chap_directory, file)) as in_file:
                         text = in_file.read()
                         sentence_text_exists = True
+                    if text.endswith('?'):
+                        sent_type = 'Q'
+                    else:
+                        sent_type = 'S'
+
                     if not sentence_text_exists:
                         return print('Error: missing text in baseline')
-                if dirname.startswith('Chapter'):
-                    # Extract the name and path of wav file
-                    if file.endswith('.wav'):
-                        audio_path = os.path.join(chap_directory, file)
 
-                    elif file.endswith('.TextGrid'):
-                        textgrid_path = os.path.join(chap_directory, file)
-                    elif file.endswith('.png'):
-                        textplot_path = os.path.join(chap_directory, file)
-                elif dirname.startswith('Baseline'):
-                    textplot_path = 'Baseline'
-                    audio_path = 'Baseline'
+                # Extract the name and path of wav file
+                elif file.endswith('.wav'):
+                    audio_path = os.path.join(chap_directory, file)
+
+                elif file.endswith('.TextGrid'):
+                    textgrid_path = os.path.join(chap_directory, file)
+                elif file.endswith('.png'):
+                    textplot_path = os.path.join(chap_directory, file)
+
+                elif file == '.DS_Store':
+                    pass
                 else:
                     return print("Error: filesystem corrupt")
 
-            if title and text:
+            if sent_id and text:
                 # TODO: get new variables from schema
                 database.execute(
-                    'INSERT INTO chapters (chapter_title, audio_path, textplot_path, text)'
-                    ' VALUES (?, ?, ?, ?)',
-                    (title, audio_path, textplot_path, text)
+                    'INSERT INTO chapters (sent_group, sent_type, sent_id, text, audio_path, textplot_path)'
+                    ' VALUES (?, ?, ?, ?, ?, ?)',
+                    (sent_group, sent_type, sent_id, text, audio_path, textplot_path)
                 )
                 database.commit()
 
