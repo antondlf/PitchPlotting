0137cf81af270ad58bc127c5961a108e09e9f596
Antón <antondlf@mac.com>
2021-08-09 18:51:23 +0200

    Added a shell command with an argument as path to txt file with email list and starts email reminders automatically

--- flaskr/auto_email.py
+++ flaskr/auto_email.py
@@ -5,22 +5,60 @@ from future.backports.email.mime.text import MIMEText
 # Part of this code based on https://towardsdatascience.com/automate-email-with-python-1e755d9c6276
 # and https://realpython.com/python-send-email/
 
+#def session_text(session, reminder):
+def contains_non_ascii_characters(str):
+    return not all(ord(c) < 128 for c in str)
 
-def generate_email(stage):
-    if stage == 'Session 1':
-        text = MIMEText("Hello! \n\n" \
-               "This is an example automated email:"
-                        "Welcome to Italian Prosody Training!\n" \
-               "Here you will learn how Native Italian Speakers use their intonation, and you will have a chance to compare it to yours." \
-               "To get started, go to the following url..." \
-               "We hope you enjoy our training!\n\n" \
-               "If you run into any issues, contact ...\n\n\n" \
-               "Regards \n\n\n" \
-               "Antón de la Fuente and Catherine Scanlon")
-        msg = MIMEMultipart()
-        msg['Subject'] = 'Italian Prosody Project Part 1'
-        msg.attach(text)
-        return msg
+
+def generate_email(stage, is_reminder=False, is_reminder_2=False):
+
+    if is_reminder:
+        reminder = ' reminder'
+        text = MIMEText("Hello!\n\n"
+                        "This is an automated email\n\n"
+                        "This is a reminder that you still have not completed" + stage + "for the Italian prosody learning course\n"
+                        "Please complete the course at your earliest convenience,"
+                        "You will receive one more reminder in case you can't complete this in the next two days.\n"
+                        "If cannot complete the rest of the study or would rather unenroll, let either of the contact emails know.\n\n"
+                        "Thank you for your contribution to our study,\n\n"
+                                                                                         "Regards,\n\n"
+                                                                                         "Antón de la Fuente and Catherine Scanlon")
+    elif is_reminder_2:
+        reminder = ' last reminder'
+        text = MIMEText("Hello!\n\n"
+                        "This is an automated email\n\n"
+                        "This is a reminder that you still have not completed " + stage + " for the Italian prosody learning course\n"
+                        "Please complete the course at your earliest convenience."
+                        "You will not receive any more reminders for this stage, but if you do not intend to finish the course please\n"
+                        "notify us at either of the contact emails.\n\n"
+                        "Thank you for your contribution to our study,\n\n"
+                                                                                         "Regards,\n\n"
+                                                                                         "Antón de la Fuente and Catherine Scanlon")
+    else:
+        reminder = ''
+        if stage == 'Session 1':
+
+            # TODO: get session specific urls
+            url = 'prosody.delafuentealvarez.com'
+            text = MIMEText("Hello! \n\n" \
+                   "This is an example automated email:\n"
+                            "Welcome to Italian Prosody Training.\n" \
+                   "Here you will learn how Native Italian Speakers use their intonation, and you will have a chance to compare it to yours." \
+                   "To get started, go to the following url:",  url, \
+                   "We hope you enjoy our training module. \n\n" \
+                   "If you run into any issues, contact ...\n\n\n" \
+                   "Regards \n\n\n" \
+                   "Antón de la Fuente and Catherine Scanlon")
+
+    if (contains_non_ascii_characters(text)):
+        plain_text = MIMEText(text.encode('utf-8'), 'plain', 'utf-8')
+    else:
+        plain_text = MIMEText(text, 'plain')
+
+    msg = MIMEMultipart()
+    msg['Subject'] = 'Italian Prosody Project ' + stage + reminder
+    msg.attach(plain_text)
+    return msg
     
 
 def send_email(msg, sender, receiver):
@@ -39,6 +77,12 @@ def send_email(msg, sender, receiver):
         smtp.sendmail(sender,
                       receiver,
                       msg.as_string())
+
+def notify(session, receiver):
+
+    msg = generate_email(session)
+    send_email(msg, 'testdummyprosody@gmail.com', receiver)
+
 
 def main():
     msg = generate_email('Session 1')
---
+++ flaskr/email_management.py
@@ -0,0 +1,111 @@
+import os
+import click
+import random
+import diceware
+from flask import app, Blueprint
+from flask.cli import with_appcontext
+from flaskr.db import get_db
+from werkzeug.security import generate_password_hash
+from pitch_track.user_dict import create_user_dict
+from flaskr.auto_email import notify
+
+bp = Blueprint('notify', __name__, url_prefix='/notify')
+
+def read_email_list(email_list):
+
+    with open(email_list, 'r') as in_file:
+        # TODO: add encryption
+        emails = in_file.read().splitlines()
+        print(emails)
+
+    return emails
+
+
+def input_email_db(username, email):
+    db = get_db()
+
+    db.execute(
+        'INSERT INTO email (username, email)'
+        ' VALUES (?, ?)',
+        (username, email)
+    )
+    db.commit()
+
+
+def register_account(username, password):
+
+    db = get_db()
+    db.execute(
+        'INSERT INTO user (username, password) VALUES (?, ?)',
+        (username, generate_password_hash(password))
+    )
+    db.commit()
+
+
+def generate_group(group_list, username_list, db):
+
+    for i in range(len(group_list)):
+
+        # Create a username and password
+        username = username_list[i]
+        password = diceware.get_passphrase()
+
+        # Input into database
+        input_email_db(username, group_list[i])
+        register_account(username, password)
+
+        # Generate user_dict
+        user_id = db.execute(
+            'SELECT id FROM user WHERE username=?',
+            (username,)
+        ).fetchall()[0]['id']
+        create_user_dict(user_id)
+
+
+def create_accounts(email_list):
+
+    # Shuffle emails
+    print(email_list)
+    random.shuffle(email_list)
+    print(email_list)
+
+    # Get two groups
+    half_len = int(len(email_list)/2)
+    print(half_len)
+
+    # Get two random groups
+    group_a = email_list[:half_len]
+    group_b = email_list[half_len:]
+
+    db = get_db()
+
+    # Get random usernames
+    with open(diceware.get_wordlist_path('en')) as in_file:
+        wordlist = in_file.read().splitlines()
+        username_list = random.choices(wordlist, k=len(email_list))
+
+    # Apportion usernames to groups
+    usernames_a = username_list[:half_len]
+    usernames_b = username_list[half_len:]
+
+    # Generate groups
+    generate_group(group_a, usernames_a, db)
+    generate_group(group_b, usernames_b, db)
+
+
+
+@click.command('start-experiment')
+@click.argument('email_list', type=click.Path(exists=True))
+@with_appcontext
+def start_experiment(email_list):
+
+    emails = read_email_list(email_list)
+    create_accounts(emails)
+    for email in emails:
+        notify('Session 1', emails)
+
+    click.echo('Users registered and notifications sent.')
+
+
+def init_notification_system(app):
+    app.cli.add_command(start_experiment)
