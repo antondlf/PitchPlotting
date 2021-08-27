6f524d6fe1f8bf741461c4491aa826a91c46a5c3
Antón <antondlf@mac.com>
2021-08-28 00:50:11 +0200

    Fixed imports again

--- site/flaskr/audio_processing.py
+++ site/flaskr/audio_processing.py
@@ -4,7 +4,7 @@ from flask import (
 
 from flaskr.db import get_db
 
-from flaskr.pitch_track.pitch_plot import draw_pitch
+from pitch_track.pitch_plot import draw_pitch
 
 from flaskr.data_management import get_unique_id
 
