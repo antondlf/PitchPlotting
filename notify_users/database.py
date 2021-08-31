228fb5089f1eba94405a7cb9238a1539b7a0d272
Antón <antondlf@mac.com>
2021-08-31 13:34:21 +0200

    Changed db reading paths

--- notify_users/database.py
+++ notify_users/database.py
@@ -4,7 +4,7 @@ import os
 
 def init_email_db():
 
-    db = sqlite3.connect('./backend_instance/base.sqlite')
+    db = sqlite3.connect('../backend_instance/base.sqlite')
 
     with open('./notify_users/email_schema.sql') as f:
         db.executescript(f.read())
@@ -13,14 +13,14 @@ def init_email_db():
 
 def get_flaskr_db():
 
-    if os.path.isfile('./site/instance/flaskr.sqlite'):
+    if os.path.isfile('../site/instance/flaskr.sqlite'):
         db = sqlite3.connect('./site/instance/flaskr.sqlite',
         detect_types=sqlite3.PARSE_DECLTYPES
         )
         db.row_factory = sqlite3.Row
     else:
-        db = sqlite3.connect('./site/instance/flaskr.sqlite')
-        with open('./site/flaskr/schema.sql') as f:
+        db = sqlite3.connect('../site/instance/flaskr.sqlite')
+        with open('../site/flaskr/schema.sql') as f:
             db.executescript(f.read())
         db.commit()
     return db
@@ -28,14 +28,14 @@ def get_flaskr_db():
 
 def connect_email_db():
 
-    if os.path.isfile('./backend_instance/base.sqlite'):
-        db = sqlite3.connect('./backend_instance/base.sqlite',
+    if os.path.isfile('../backend_instance/base.sqlite'):
+        db = sqlite3.connect('../backend_instance/base.sqlite',
         detect_types=sqlite3.PARSE_DECLTYPES
         )
         db.row_factory = sqlite3.Row
     else:
         db = sqlite3.connect('./backend_instance/base.sqlite')
-        with open('./notify_users/email_schema.sql') as f:
+        with open('../notify_users/email_schema.sql') as f:
             db.executescript(f.read())
         db.commit()
     return db
